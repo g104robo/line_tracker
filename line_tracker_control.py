c5ddefbc1f35a57fc879ab53803926b0ee9dcd2b
g104robo <gokuraku104robot@gmail.com>
2017-11-09 17:20:41 +0900

    add odeint

--- line_tracker_control.py
+++ line_tracker_control.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 from control.matlab import *
 import numpy as np
+import math as math
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
 # k1 = 4.0
 # k2 = 3.0
 # k3 = 2.0
 # Vd = 1.0
+
+phi0 = [ math.pi/6, 0.0, 0.0]
+Vd = 1.0
+
+# def func(phi, k, Vd):
+#     ret = [phi[1], phi[2], k[3]*Vd*phi[0] + k[1]*phi[1] + k[2]*phi[2]]
+#     return ret
+
+def func(phi, time, k1, k2, k3, Vd):
+    ret = [phi[1], phi[2], -k3*Vd*phi[0] - k1*phi[1] - k2*phi[2]]
+    return ret
 
 def check_ctrb(A,B):
     Uc = ctrb(A,B)  #可制御性行列の計算
@@ -26,10 +40,18 @@ def main():
     poles1 = [-1,-1+1j,-1-1j]
     poles2 = [-2,-2+1j,-2-1j]
     #ゲインの設計（極配置法）
-    F = place(A,B,poles2)
+    F = place(A,B,poles1)
     #計算結果の表示
     print("gain:",F)
     print("poles:", np.linalg.eigvals(A-B*F))
+    # print(F[0][0])
+    # print(F[0][1])
+    # print(F[0][2])
+    time = np.linspace(0,50,1000)
+    phi_out = odeint(func, phi0, time, args=(F[0][0],F[0][1],F[0][2],Vd))
+    print(phi_out)
+    plt.plot(time, phi_out)
+    plt.show()
 
 if __name__ == "__main__":
     main()
