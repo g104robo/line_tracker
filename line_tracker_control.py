69df9e648469df85c7af998e0b75eabbf3158a53
g104robo <gokuraku104robot@gmail.com>
2017-11-15 20:35:30 +0900

    add eta output. but phi and eta do not work

--- line_tracker_control.py
+++ line_tracker_control.py
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 # k3 = 2.0
 # Vd = 1.0
 
-phi0 = [1.0, math.pi/6, 0.0, 0.0]
+phi0 = [math.pi/6, 0.0, 0.0]
+eta0 = [1.0, math.pi/6, 0.0, 0.0]
 #eta0 = 1.0
 Vd = 1.0
 
@@ -19,7 +20,11 @@ Vd = 1.0
 #     return ret
 
 def func_phi(phi, time, k1, k2, k3, Vd):
-    ret = [Vd*phi[0], phi[2], phi[3], -k3*Vd*phi[1] - k1*phi[2] - k2*phi[3]]
+    ret = [phi[0], phi[2], -k3*Vd*phi[0] - k1*phi[1] - k2*phi[2]]
+    return ret
+
+def func_eta(eta, time, k1, k2, k3, Vd):
+    ret = [Vd*eta[0], eta[2], eta[3], -k3*Vd*eta[1] - k1*eta[2] - k2*eta[3]]
     return ret
 
 # def func_phi(phi, time, k1, k2, k3, Vd):
@@ -42,6 +47,10 @@ def main():
     #A = np.array([[0,1,0],[0,0,1],[k3*Vd, k1, k2]])
     A = np.array([[0,1,0],[0,0,1],[0, 0, 0]])
     B = np.array([[0],[0],[1]])
+
+    etaA = np.array([[0,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]])
+    etaB = np.array([[0],[0],[0],[1]])
+
     #システムが可制御でなければ終了
     if check_ctrb(A,B) == -1 : exit
     #所望の極
@@ -49,6 +58,12 @@ def main():
     poles2 = [-2,-2+1j,-2-1j]
     poles3 = [-2,-2+5j,-2-5j]
     poles4 = [-1,-5+5j,-5-5j]
+    
+    # eta_poles1 = [-1,-1,-1+1j,-1-1j]
+    # eta_poles2 = [-1,-2,-2+1j,-2-1j]
+    # eta_poles3 = [-1,-2,-2+5j,-2-5j]
+    # eta_poles4 = [-1,-1,-5+5j,-5-5j]
+
     #ゲインの設計（極配置法）
     F = place(A,B,poles1)
     #計算結果の表示
@@ -59,13 +74,14 @@ def main():
     # print(F[0][2])
     time = np.linspace(0,30,1000)
     phi_out = odeint(func_phi, phi0, time, args=(F[0][0],F[0][1],F[0][2],Vd))
+    eta_out = odeint(func_eta, eta0, time, args=(F[0][0],F[0][1],F[0][2],Vd))
+    
     #eta_out = odeint(func_eta, eta0, time, args=(Vd,))
     #print(phi_out[:, 0])
-    plt.plot(time, phi_out[:, 0], label="eta")
-    plt.plot(time, phi_out[:, 1], label="phi")
-    plt.plot(time, phi_out[:, 2], label="ang_vel")
-    plt.plot(time, phi_out[:, 3], label="ang_acc")
-    plt.plot(time, eta_out, label="eta")
+    plt.plot(time, phi_out[:, 0], label="phi")
+    plt.plot(time, phi_out[:, 1], label="ang_vel")
+    plt.plot(time, phi_out[:, 2], label="ang_acc")
+    plt.plot(time, eta_out[:, 0], label="eta")
     plt.legend()
     plt.show()
 
